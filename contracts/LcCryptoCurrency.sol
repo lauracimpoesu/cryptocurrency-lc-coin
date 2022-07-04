3a3a24f780b39fda4690f98598dccb70ef526030
Laura Cimpoesu <80644410+lauracimpoesu@users.noreply.github.com>
2022-07-04 22:02:26 +0200

    uniswap token

---
+++ contracts/LcCryptoCurrency.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract LcCryptoCurrency is ERC20, Ownable {
+    constructor() ERC20("LcCryptoCurrency", "LCCC") {
+        _mint(msg.sender, 10000 * 10 ** decimals());
+    }
+}
---
+++ hardhat.config.js
@@ -0,0 +1,32 @@
+require("@nomiclabs/hardhat-waffle");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+const privateKey1 =
+  "5422ba3f4264b199a09f493059b3d9b348b55480081a66423df1b2caaef710cd"
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.4",
+  defaultNetwork: "rinkeby",
+  networks: {
+    hardhat: {},
+    rinkeby: {
+      url: "https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
+      accounts: [privateKey1],
+    },
+  },
+}
